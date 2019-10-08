85a00535d16ced53b4b3dea8f7912744d27595cd
Open Close <interfiancee@gmail.com>
2019-10-09 00:29:24 +0800

    Refactor CommandResult to be more extension-friendly.

--- src/main/java/seedu/mark/logic/commands/HelpCommand.java
+++ src/main/java/seedu/mark/logic/commands/HelpCommand.java
@@ -1,5 +1,6 @@
 package seedu.mark.logic.commands;
 
+import seedu.mark.logic.commands.commandresult.CommandResult;
 import seedu.mark.model.Model;
 
 /**
@@ -16,6 +17,6 @@ public class HelpCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        return new CommandResult(SHOWING_HELP_MESSAGE);
     }
 }
